16280040f8fc2f973c43e6fa6ae5eb2d949e21ec
Radoslaw Adamczyk <greatpolandweb@gmail.com>
2021-08-01 14:47:54 +0200

    add rainfall  styles

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import styles from './Location.module.scss';
-import {Lang} from './Language/Language';
 import {Search} from './Search/Search';
 
 export const Location = () => {
 
     return(
-        <>
-        <Lang/>
-            
         <header className={styles.header}>
             <Search/>
         </header>
-        </>       
     )
 }
--- src/components/Result/Rainfall/Rainfall.js
+++ src/components/Result/Rainfall/Rainfall.js
@@ -1,107 +1,62 @@
 import React from 'react';
+import { FiSun as SunIcon } from "react-icons/fi";
+import styles from './Rainfall.module.scss';
+
 
 export const Rainfall = () => {
     return (
-         <article >
-        <div>
-          <p>
-              Prędkość wiatru {/* wind.speed.value */}
-              <br />
-              0.93
-            </p>
-            <p>
-              Jednostka prędkości wiatru {/* wind.speed.unit */}
-              <br />
-              m/s
-            </p>
-            <p>
-              Nazwa wiatru {/* wind.speed.name */}
-              <br />
-              calm
-          </p>    
-        </div>
-        <hr/>
-        <div>
-          <p>
-              Kierunek wiatru {/* wind.direction.value */}
-              <br />
-              23
-            </p>
-            <p>
-              Kod kierunku wiatru {/* wind.direction.code */}
-              <br />
-              NNE
-            </p>
-            <p>
-              Pełna nazwa kodu kierunku wiatru {/* wind.direction.name */}
-              <br />
-              North-northeast
-          </p>    
-        </div>
-        <hr />
-        <div>
-        <p>
-            Zachmurzenie {/* clouds.value  */}
-            <br />
-            1
-          </p>
-          <p>
-            Nazwa zachmurzenia {/* clouds.name */}
-            <br />
-            Clear sky
-          </p>
-          <p>
-            Widoczność {/* visibility.value  */}
-            <br />
-            16093
-        </p>    
-        </div>
-        <hr />
-        <div>
-          <p>
-              Opady {/* precipitation.value  */}
-              <br />
-              sadasddsa
-            </p>
-            <p>
-              Warunki pogodowe {/* precipitation.mode */}
-              <br />
-              no
-            </p>
-          </div>
-          <hr />
-           <div>
-        <p>
-            Objętość deszczu w ciągu ostatniej godziny{/* rain.1h */}
-            <br />
-            1 m<sup>3</sup>
-          </p>
-          <p>
-            Objętość śniegu w ciągu ostatniej godziny {/* snow.1h */}
-            <br />
-            1 m<sup>3</sup>
-          </p>
-      
-        </div>
-       
-        <hr />
-        <div>
-        <p>
-            Identyfikator warunków pogodowych {/* weather.value  */}
-            <br />
-            800
-          </p>
-          <p>
-            Nazwa warunków pogodowych {/* weather.name */}
-            <br />
-            Clear sky
-          </p>
-          <p>
-            Identyfikator ikony pogody {/* weather.icon  */}
-            <br />
-            01n
-        </p>    
-        </div>
-       </article>
+
+       <article className={styles.rain}>
+          <section className={styles.rain__wrapper}>
+            <div className={styles.rain__wrapper__item}>
+              <b className={styles.rain__wrapper__item__title}>Wind speed</b>
+              <p className={styles.rain__wrapper__item__text}>0.93 m/s</p>
+            </div>
+            <div className={styles.rain__wrapper__item}>
+              <b className={styles.rain__wrapper__item__title}>Wind name</b>
+              <p className={styles.rain__wrapper__item__text}>calm</p>
+            </div>
+            <div className={styles.rain__wrapper__item}>
+              <b className={styles.rain__wrapper__item__title}>Wind direction</b>
+              <p className={styles.rain__wrapper__item__text}>23</p>
+            </div>
+            <div className={styles.rain__wrapper__item}>
+              <b className={styles.rain__wrapper__item__title}>Wind direction name</b>
+              <p className={styles.rain__wrapper__item__text}>North-northeast</p>
+            </div>
+          </section>
+        
+
+          <section className={styles.rain__wrapper}>
+               <div className={styles.rain__wrapper__item}>
+                <b className={styles.rain__wrapper__item__title}>Clouds</b>
+                <p className={styles.rain__wrapper__item__text}>Clear sky</p>
+              </div>
+              <div className={styles.rain__wrapper__item}>
+                <b className={styles.rain__wrapper__item__title}>Visibility</b>
+                <p className={styles.rain__wrapper__item__text}>16093</p>
+              </div>
+            </section>
+         <section className={styles.rain__wrapper}>
+               <div className={styles.rain__wrapper__item}>
+                <b className={styles.rain__wrapper__item__title}>Precipitation</b>
+                <p className={styles.rain__wrapper__item__text}>clear</p>
+              </div>
+              <div className={styles.rain__wrapper__item}>
+                <b className={styles.rain__wrapper__item__title}>Weather icon</b>
+                <p className={styles.rain__wrapper__item__text}><SunIcon/></p>
+              </div>
+            </section>
+         <section className={styles.rain__wrapper}>
+               <div className={styles.rain__wrapper__item}>
+                <b className={styles.rain__wrapper__item__title}>The volume of rain during 1 hour</b>
+                <p className={styles.rain__wrapper__item__text}>1 m<sup>3</sup></p>
+              </div>
+              <div className={styles.rain__wrapper__item}>
+                <b className={styles.rain__wrapper__item__title}>The volume of snow during 1 hour</b>
+                <p className={styles.rain__wrapper__item__text}>1 m<sup>3</sup></p>
+              </div>
+            </section>
+        </article>
     )
 }
