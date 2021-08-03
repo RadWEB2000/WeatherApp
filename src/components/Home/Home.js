bc25872ff047e51d24efebdfcf5022fc923ad791
Radoslaw Adamczyk <greatpolandweb@gmail.com>
2021-08-03 11:26:57 +0200

    finish

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -37,7 +37,7 @@ export class Home extends Component {
         e.preventDefault();
         const searchValue = this.refs.city.value;
         console.log(searchValue);
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&APPID=${weatherKey}&units=metric`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&APPID=${weatherKey}&units=metric&lang=en`)
             .then(res => res.json())
             .then(data => {
                 console.log(data);
@@ -93,6 +93,7 @@ export class Home extends Component {
         const sunriseTime = new Date(this.state.sunrise * 1000).toLocaleTimeString;
         const sunsetTime = new Date(this.state.sunset * 1000).toLocaleTimeString;
 
+
         return (
             <>
             <header className={styles.header}>
@@ -116,94 +117,97 @@ export class Home extends Component {
                         <SearchIcon className={styles.form__button__icon}/>
                     </button>
                 </form>
-            </header>
-            <main className={styles.result}>
-                <LoadScript
-                    googleMapsApiKey="AIzaSyCbwcH7qQ7r__ATB5vJddSKpSYXX0Z-5wY"
-                    className={styles.map}
-                >
-                    <GoogleMap
-                        mapContainerStyle={mapProps.style}
-                        center={mapProps.center}
-                        zoom={mapProps.zoom}
-                        options={{mapId:'9cf98a4c24218f39'}}
-                    >   
-                        <Marker
-                            position={mapProps.center}
-                        />
-                    </GoogleMap>
-                </LoadScript>
-                <h1 className={styles.result__title}> {this.state.name}</h1>
-                <div className={styles.result__wrapper}>
-                     <article className={styles.city}>
-                        <section className={styles.city__item}>
-                            <b className={styles.city__item__name}>Zip code</b>
-                            <p className={styles.city__item__value}> {this.state.country}</p>
-                        </section>   
-                        <section className={styles.city__item}>
-                            <b className={styles.city__item__name}>Short weahter</b>
-                            <p className={styles.city__item__value}> {this.state.description}</p>
-                        </section>   
-                        <section className={styles.city__item}>
-                            <b className={styles.city__item__name}>City id</b>
-                            <p className={styles.city__item__value}>{this.state.id}</p>
-                        </section>   
-                    </article>
-                    <article className={styles.time}>
-                        <section className={styles.time__item}>
-                            <b className={styles.time__item__name}>Sunrise</b>
-                            <p className={styles.time__item__value}> {sunriseTime}</p>
-                        </section>   
-                        <section className={styles.time__item}>
-                            <b className={styles.time__item__name}>Timezone</b>
-                            <p className={styles.time__item__value}> {this.state.timezone}</p>
-                        </section>   
-                        <section className={styles.time__item}>
-                            <b className={styles.time__item__name}>Sunset</b>
-                            <p className={styles.time__item__value}> {sunsetTime}</p>
-                        </section>   
-                    </article>
-                     <article className={styles.temp}>
-                        <section className={styles.temp__item}>
-                            <b className={styles.temp__item__name}>Temperature</b>
-                            <p className={styles.temp__item__value}>{this.state.temp}°C</p>
-                        </section>   
-                        <section className={styles.temp__item}>
-                            <b className={styles.temp__item__name}>Feel Temperature</b>
-                            <p className={styles.temp__item__value}>{this.state.tempLike}°C</p>
-                        </section>  
-                        <section className={styles.temp__item}>
-                            <b className={styles.temp__item__name}>Min Temperature</b>
-                            <p className={styles.temp__item__value}>{this.state.tempMin}°C</p>
-                        </section>  
-                        <section className={styles.temp__item}>
-                            <b className={styles.temp__item__name}>Max Temperature</b>
-                            <p className={styles.temp__item__value}>{this.state.tempMax}°C</p>
-                        </section>  
-                    </article>
-                    <article className={styles.clouds}>
-                        <section className={styles.clouds__item}>
-                            <b className={styles.clouds__item__name}>Clouds</b>
-                            <p className={styles.clouds__item__value}> {this.state.clouds}</p>
-                        </section>   
-                        <section className={styles.clouds__item}>
-                            <b className={styles.clouds__item__name}>Visibility</b>
-                            <p className={styles.clouds__item__value}> {this.state.visibility}%</p>
-                        </section>  
-                    </article>
-                    <article className={styles.wind}>
-                        <section className={styles.wind__item}>
-                            <b className={styles.wind__item__name}>Wind speed</b>
-                            <p className={styles.wind__item__value}>{this.state.windSpeed}m/s</p>
-                        </section>   
-                        <section className={styles.wind__item}>
-                            <b className={styles.wind__item__name}>Wind direction</b>
-                            <p className={styles.wind__item__value}>{this.state.windDirection}</p>
-                        </section>  
-                    </article>
+                </header>
+                
 
-                </div>
-            </main>
+
+                    <main className={styles.result}>
+                        <LoadScript
+                            googleMapsApiKey="AIzaSyCbwcH7qQ7r__ATB5vJddSKpSYXX0Z-5wY"
+                            className={styles.map}
+                        >
+                            <GoogleMap
+                                mapContainerStyle={mapProps.style}
+                                center={mapProps.center}
+                                zoom={mapProps.zoom}
+                                options={{mapId:'9cf98a4c24218f39'}}
+                            >   
+                                <Marker
+                                    position={mapProps.center}
+                                />
+                            </GoogleMap>
+                        </LoadScript>
+                        <h1 className={styles.result__title}> {this.state.name}</h1>
+                        <div className={styles.result__wrapper}>
+                            <article className={styles.city}>
+                                <section className={styles.city__item}>
+                                    <b className={styles.city__item__name}>Zip code</b>
+                                    <p className={styles.city__item__value}> {this.state.country}</p>
+                                </section>   
+                                <section className={styles.city__item}>
+                                    <b className={styles.city__item__name}>Short weahter</b>
+                                    <p className={styles.city__item__value}> {this.state.description}</p>
+                                </section>   
+                                <section className={styles.city__item}>
+                                    <b className={styles.city__item__name}>City id</b>
+                                    <p className={styles.city__item__value}>{this.state.id}</p>
+                                </section>   
+                            </article>
+                            <article className={styles.time}>
+                                <section className={styles.time__item}>
+                                    <b className={styles.time__item__name}>Sunrise</b>
+                                    <p className={styles.time__item__value}> {this.state.sunrise}</p>
+                                </section>   
+                                <section className={styles.time__item}>
+                                    <b className={styles.time__item__name}>Timezone</b>
+                                    <p className={styles.time__item__value}> {this.state.timezone}</p>
+                                </section>   
+                                <section className={styles.time__item}>
+                                    <b className={styles.time__item__name}>Sunset</b>
+                                    <p className={styles.time__item__value}> {this.state.sunset}</p>
+                                </section>   
+                            </article>
+                            <article className={styles.temp}>
+                                <section className={styles.temp__item}>
+                                    <b className={styles.temp__item__name}>Temperature</b>
+                                    <p className={styles.temp__item__value}>{this.state.temp}°C</p>
+                                </section>   
+                                <section className={styles.temp__item}>
+                                    <b className={styles.temp__item__name}>Feel Temperature</b>
+                                    <p className={styles.temp__item__value}>{this.state.tempLike}°C</p>
+                                </section>  
+                                <section className={styles.temp__item}>
+                                    <b className={styles.temp__item__name}>Min Temperature</b>
+                                    <p className={styles.temp__item__value}>{this.state.tempMin}°C</p>
+                                </section>  
+                                <section className={styles.temp__item}>
+                                    <b className={styles.temp__item__name}>Max Temperature</b>
+                                    <p className={styles.temp__item__value}>{this.state.tempMax}°C</p>
+                                </section>  
+                            </article>
+                            <article className={styles.clouds}>
+                                <section className={styles.clouds__item}>
+                                    <b className={styles.clouds__item__name}>Clouds</b>
+                                    <p className={styles.clouds__item__value}> {this.state.clouds}</p>
+                                </section>   
+                                <section className={styles.clouds__item}>
+                                    <b className={styles.clouds__item__name}>Visibility</b>
+                                    <p className={styles.clouds__item__value}> {this.state.visibility}%</p>
+                                </section>  
+                            </article>
+                            <article className={styles.wind}>
+                                <section className={styles.wind__item}>
+                                    <b className={styles.wind__item__name}>Wind speed</b>
+                                    <p className={styles.wind__item__value}>{this.state.windSpeed}m/s</p>
+                                </section>   
+                                <section className={styles.wind__item}>
+                                    <b className={styles.wind__item__name}>Wind direction</b>
+                                    <p className={styles.wind__item__value}>{this.state.windDirection}</p>
+                                </section>  
+                            </article>
+
+                        </div>
+                    </main>
             </>
         )
     }
